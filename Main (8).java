6cf473af2d4b404943dc0bdc5ee7099e1e34563a
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-11-17 22:33:36 -0300

    Aluna: Helena Patrícia de Souza Silva
    Disciplina: Linguagem de Programação I 
    Data: 17/11/2021

---
+++ Main (8).java
@@ -0,0 +1,31 @@
+//questão 3//
+
+ public class {
+    
+    
+    public HashMap<int,int> getMap(){
+        
+        aluno1 = 5 , 120.302.514-36;
+        aluno2 = 6 , 133.002.525-66;
+        aluno3 = 8 , 011.312.444-33;
+        aluno4 = 9 , 155.666.654-58;
+        aluno5 = 10 , 764.605.203-56;
+        .
+        .
+        .
+        aluno50 = 7 , 365.145.258-98; 
+        
+        System.out.println("Aluno com o CPF 764.605.203-56:"+ aluno5);
+    } 
+ }
+    
+
+            
+            
+        
+            
+            
+            
+            
+            
+    
